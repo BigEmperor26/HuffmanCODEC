53ab09dab24f6985b80a0bd5d2ef97a83a3f843b
Michele Yin <mickyin3@gmail.com>
2022-11-03 17:35:35 +0100

    dumb copy and paste edits

--- src/read.c
+++ src/read.c
@@ -5,6 +5,80 @@
 #include "priorityQ.h"
 #include <mpi.h>
 
+// int parseFileListFromFind(int readDescriptor, NamesList *fileList){
+//     FILE *pipeToRead = fdopen(readDescriptor, "r");
+//     int numOfFileNamesProcessed = 0;
+//     char buffer[SIZE_OF_BUFFER_TO_READ_PIPE];
+//     string filePath;
+
+//     while(fgets(buffer, SIZE_OF_BUFFER_TO_READ_PIPE, pipeToRead) != NULL){
+//         filePath = strtok(buffer, "\n");
+//         absolutePath = realpath(filePath, absolutePath);
+//         if(absolutePath != NULL){
+//             appendNameToNamesList(fileList, absolutePath);
+//         }
+//         numOfFileNamesProcessed++;
+//     }
+
+//     return numOfFileNamesProcessed;
+// }
+
+// // Get all the file path inside a folder
+// // Error codes:
+// // 1: it was not possible to fork a child
+// // 2: it was not possibile to inspect the folder
+// int crawler(string folder, NamesList *fileList, int* outNumFilesFound){
+//     int returnCode = 0;
+//     *outNumFilesFound = -1; // in case of error;
+//     string lsArgs[] = {"find", folder, "-type", "f", NULL};
+//     int fds[2];
+//     pipe(fds);
+
+//     pid_t f = fork();
+//     if (f < 0){
+//         fprintf(stderr, "\nError creating little brittle crawler-son\n");
+//         returnCode = 1;
+//     } else if (f == 0){
+//         // child
+//         close(fds[READ]);
+//         dup2(fds[WRITE], 1); // substitute stdout with fds[WRITE] for ls
+//         execvp("find", lsArgs);
+//         fprintf(stderr, "Error: it is not possibile to inspect folder: %s\n", folder);
+//         returnCode = 2; // should never be here if exec works fine
+//     } else {
+//         // parent
+//         close(fds[WRITE]);
+        
+//         // TODO check out == 0
+//         int out; // return code from ls
+//         pid_t pid = waitpid(f, &out, 0);
+//         if (pid != -1){
+//             *outNumFilesFound = parseFileListFromFind(fds[READ], fileList);
+//         } else {
+//             fprintf(stderr, "Error waiting for ls syscall termination\n");
+//             returnCode = 3;
+//         }
+//     }
+
+//     return returnCode;
+// }
+// // Given a path to a file/folder it returns:
+// // -1 : if it does not exist
+// //  0 : if it is a file and it exists
+// //  1 : if it is is a folder and it exists
+// int inspectPath(const char *path){
+//     struct stat path_stat;
+//     int returnCode = -1;
+//     if (path != NULL && stat(path, &path_stat) == 0){
+//         if (S_ISREG(path_stat.st_mode)){
+//             returnCode = 0;
+//         } else if (S_ISDIR(path_stat.st_mode)){
+//             returnCode = 1;
+//         }
+//     }
+//     return returnCode;
+// }
+
 
 // the key is the char as integer
 // size is the number of different character or bytes, so 1 byte = 256
@@ -42,17 +116,16 @@ void printDictionary(Dictionary * d){
     return;
 }
 
-// with offset each partition is up to 4gb
-int counter(MPI_File *readfile, Dictionary *d, ull offset, ull size){
+// with offset each partition is up to 2^64
+int charCounter(MPI_File *readfile, Dictionary *d, ull offset, ull size){
     // with binary offset
     char c;
-    ull count = 0;
     ull read = 0;
     MPI_File_seek(*readfile, offset, MPI_SEEK_SET);
-    while ((MPI_File_read(*readfile, &c, 1, MPI_CHAR, MPI_STATUS_IGNORE) == MPI_SUCCESS) && (count<size)){
+    while ((MPI_File_read(*readfile, &c, 1, MPI_CHAR, MPI_STATUS_IGNORE) == MPI_SUCCESS) && (read<size)){
         // printf("rank %d read %c \n",rank, c);
         d->frequency[c]++;
-        count++;
+        read++;
     }
     return 0;
 }
@@ -117,7 +190,7 @@ int main(int argc, char ** argv) {
     Dictionary * d = createDictionary(256);
     // count the frequency of each character
     if (rank<active_processes){
-        counter(&readfile, d, offset, size_to_read);
+        charCounter(&readfile, d, offset, size_to_read);
     }
     MPI_File_close(&readfile);
 
@@ -128,5 +201,5 @@ int main(int argc, char ** argv) {
     }
     freeDictionary(d);
     MPI_Finalize();
-
+    return 0;
 }
