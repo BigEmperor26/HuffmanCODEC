4b72091f5b91d7968984434518359b589e504bd2
Michele Yin <mickyin3@gmail.com>
2022-11-03 21:27:13 +0100

    fixed unsigned char bug

--- src/charCounter.c
+++ src/charCounter.c
@@ -2,83 +2,7 @@
 
 #include "priorityQ.h"
 #include "dictionary.h"
-// #include "priorityQ.c"
-// #include "dictionary.c"
 #include <mpi.h>
-
-// int parseFileListFromFind(int readDescriptor, NamesList *fileList){
-//     FILE *pipeToRead = fdopen(readDescriptor, "r");
-//     int numOfFileNamesProcessed = 0;
-//     char buffer[SIZE_OF_BUFFER_TO_READ_PIPE];
-//     string filePath;
-
-//     while(fgets(buffer, SIZE_OF_BUFFER_TO_READ_PIPE, pipeToRead) != NULL){
-//         filePath = strtok(buffer, "\n");
-//         absolutePath = realpath(filePath, absolutePath);
-//         if(absolutePath != NULL){
-//             appendNameToNamesList(fileList, absolutePath);
-//         }
-//         numOfFileNamesProcessed++;
-//     }
-
-//     return numOfFileNamesProcessed;
-// }
-
-// // Get all the file path inside a folder
-// // Error codes:
-// // 1: it was not possible to fork a child
-// // 2: it was not possibile to inspect the folder
-// int crawler(string folder, NamesList *fileList, int* outNumFilesFound){
-//     int returnCode = 0;
-//     *outNumFilesFound = -1; // in case of error;
-//     string lsArgs[] = {"find", folder, "-type", "f", NULL};
-//     int fds[2];
-//     pipe(fds);
-
-//     pid_t f = fork();
-//     if (f < 0){
-//         fprintf(stderr, "\nError creating little brittle crawler-son\n");
-//         returnCode = 1;
-//     } else if (f == 0){
-//         // child
-//         close(fds[READ]);
-//         dup2(fds[WRITE], 1); // substitute stdout with fds[WRITE] for ls
-//         execvp("find", lsArgs);
-//         fprintf(stderr, "Error: it is not possibile to inspect folder: %s\n", folder);
-//         returnCode = 2; // should never be here if exec works fine
-//     } else {
-//         // parent
-//         close(fds[WRITE]);
-        
-//         // TODO check out == 0
-//         int out; // return code from ls
-//         pid_t pid = waitpid(f, &out, 0);
-//         if (pid != -1){
-//             *outNumFilesFound = parseFileListFromFind(fds[READ], fileList);
-//         } else {
-//             fprintf(stderr, "Error waiting for ls syscall termination\n");
-//             returnCode = 3;
-//         }
-//     }
-
-//     return returnCode;
-// }
-// // Given a path to a file/folder it returns:
-// // -1 : if it does not exist
-// //  0 : if it is a file and it exists
-// //  1 : if it is is a folder and it exists
-// int inspectPath(const char *path){
-//     struct stat path_stat;
-//     int returnCode = -1;
-//     if (path != NULL && stat(path, &path_stat) == 0){
-//         if (S_ISREG(path_stat.st_mode)){
-//             returnCode = 0;
-//         } else if (S_ISDIR(path_stat.st_mode)){
-//             returnCode = 1;
-//         }
-//     }
-//     return returnCode;
-// }
 
 
 // the key is the char as integer
@@ -159,23 +83,4 @@ bool charCounterProcess(char * filename,int rank, int size, Dictionary *d){
     // reduce the dictionary
     MPI_Reduce( (counts->frequency), d->frequency, 256, MPI_UNSIGNED_LONG_LONG, MPI_SUM, 0, MPI_COMM_WORLD);
     return true;
-}
-
-
-int main(int argc, char ** argv) {
-    MPI_Init(&argc, &argv);
-
-    char * filename = argv[1];
-    int rank, size;
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    MPI_Comm_size(MPI_COMM_WORLD, &size);
-    
-    Dictionary * counts = createDictionary(256);
-    charCounterProcess(filename, rank, size, counts);
-    if(rank==0){
-        printDictionary(counts);
-    }
-    freeDictionary(counts);
-    MPI_Finalize();
-    return 0;
 }
---
+++ src/writer.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(int argc, char *argv[]){
+    char * filename = argv[1];
+    FILE* ptr;
+    ptr = fopen(filename, "wb");
+    if (ptr == NULL) {
+        printf("Error opening file\n");
+        return 1;
+    }
+    for(int i = 0; i < 256; i++){
+        fprintf(ptr, "%c", (char) i);
+    }
+    fclose(ptr);
+}
