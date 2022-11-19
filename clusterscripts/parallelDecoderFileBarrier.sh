51f3a4fb35f7f83e0ee36153b68ee6cb6089df7c
Yin <michele.yin@hpc-head-n2.unitn.it>
2022-11-19 11:20:35 +0100

    Automatic results averager

--- clusterscripts/parallelDecoderFileBarrier.sh
+++ clusterscripts/parallelDecoderFileBarrier.sh
@@ -29,8 +29,10 @@ export OMP_NUM_THREADS=${THREADS}
 time mpiexec --report-bindings -display-devel-map --map-by node:pe=${THREADS} -np 1 ${MAIN} -${MODE} -b ${INPUTFOLDER}/${INPUT} ${OUTPUT} #1> ${OUTPUTFOLDER}/enconding_result_${THREADS}_threads  2> ${OUTPUTFOLDER}/enconding_result_${THREADS}_threads_err
 
 # How to run
-# write INPUTFOLDER=path with files to encode
-# write OUTPUTFOLDER=path to specify where to save stdout and stderr and where to save the encoded files
+# write SCRIPT='/home/michele.yin/HuffmanCODEC/clusterscripts/parallelDecoderFileLocks.sh' Position of the script
+# write INPUTFOLDER=path with files to decode
+# write OUTPUTFOLDER=path to specify where to save stdout and stderr and where to save the decoded files
+# write DECODEPARSER=/home/michele.yin/HuffmanCODEC/src/results_averager_decode.py Position of the Python parser
 # then copy and paste this command to submit the job
 # OUTPUTFOLDER will be created if it does not exist and it will contain the results of the for each run
-# nohup $(for INPUT in $(ls $INPUTFOLDER); do for RUN in $(seq 1 3); do for THREADS in 1 2 4 6 8 10 12 16 20 24; do mkdir -p ${OUTPUTFOLDER}/${INPUT}/run_${RUN} ;export THREADS; export INPUT;export INPUTFOLDER;export OUTPUTFOLDER; qsub -Wblock=true -N Decoder_run${RUN}_threads_${THREADS} -l select=1:ncpus=24:ompthreads=${THREADS}:mpiprocs=1:mem=4gb -o ${OUTPUTFOLDER}/${INPUT}/run_${RUN}/deconding_result_${THREADS}_threads -e ${OUTPUTFOLDER}/${INPUT}/run_${RUN}/deconding_result_${THREADS}_threads_err '/home/michele.yin/HuffmanCODEC/clusterscripts/parallelDecoderFileBarrier.sh'; done;done;done;)
+# nohup $(for INPUT in $(ls $INPUTFOLDER); do for RUN in $(seq 1 3); do for THREADS in 1 2 4 6 8 10 12 16 20 24; do mkdir -p ${OUTPUTFOLDER}/${INPUT}/run_${RUN} ;export THREADS; export INPUT;export INPUTFOLDER;export OUTPUTFOLDER; qsub -Wblock=true -N Decoder_run${RUN}_threads_${THREADS} -l select=1:ncpus=24:ompthreads=${THREADS}:mpiprocs=1:mem=4gb -o ${OUTPUTFOLDER}/${INPUT}/run_${RUN}/deconding_result_${THREADS}_threads -e ${OUTPUTFOLDER}/${INPUT}/run_${RUN}/deconding_result_${THREADS}_threads_err ${SCRIPT}; done;done;done; python ${DECODEPARSER} ${OUTPUTFOLDER};echo "Done")
--- clusterscripts/parallelEncoderFileBarrier.sh
+++ clusterscripts/parallelEncoderFileBarrier.sh
@@ -29,8 +29,10 @@ export OMP_NUM_THREADS=${THREADS}
 time mpiexec --report-bindings -display-devel-map --map-by node:pe=${THREADS} -np 1 ${MAIN} -${MODE} -b ${INPUTFOLDER}/${INPUT} ${OUTPUT} #1> ${OUTPUTFOLDER}/enconding_result_${THREADS}_threads  2> ${OUTPUTFOLDER}/enconding_result_${THREADS}_threads_err
 
 # How to run
+# write SCRIPT='/home/michele.yin/HuffmanCODEC/clusterscripts/parallelEncoderFileBarrier.sh' Position of the script
 # write INPUTFOLDER=path with files to encode
 # write OUTPUTFOLDER=path to specify where to save stdout and stderr and where to save the encoded files
+# write ENCODEPARSER=/home/michele.yin/HuffmanCODEC/src/results_averager_encode.py Position of the Python parser
 # then copy and paste this command to submit the job
 # OUTPUTFOLDER will be created if it does not exist and it will contain the results of the for each run
-# nohup $(for INPUT in $(ls $INPUTFOLDER); do for RUN in $(seq 1 3); do for THREADS in 1 2 4 6 8 10 12 16 20 24; do mkdir -p ${OUTPUTFOLDER}/${INPUT}/run_${RUN} ;export THREADS; export INPUT;export INPUTFOLDER;export OUTPUTFOLDER; qsub -Wblock=true -N Encoder_run${RUN}_threads_${THREADS} -l select=1:ncpus=24:ompthreads=${THREADS}:mpiprocs=1:mem=4gb -o ${OUTPUTFOLDER}/${INPUT}/run_${RUN}/enconding_result_${THREADS}_threads -e ${OUTPUTFOLDER}/${INPUT}/run_${RUN}/enconding_result_${THREADS}_threads_err '/home/michele.yin/HuffmanCODEC/clusterscripts/parallelEncoderFileBarrier.sh'; done;done;done;)
+# nohup $(for INPUT in $(ls $INPUTFOLDER); do for RUN in $(seq 1 3); do for THREADS in 1 2 4 6 8 10 12 16 20 24; do mkdir -p ${OUTPUTFOLDER}/${INPUT}/run_${RUN} ;export THREADS; export INPUT;export INPUTFOLDER;export OUTPUTFOLDER; qsub -Wblock=true -N Encoder_run${RUN}_threads_${THREADS} -l select=1:ncpus=24:ompthreads=${THREADS}:mpiprocs=1:mem=4gb -o ${OUTPUTFOLDER}/${INPUT}/run_${RUN}/enconding_result_${THREADS}_threads -e ${OUTPUTFOLDER}/${INPUT}/run_${RUN}/enconding_result_${THREADS}_threads_err ${SCRIPT}; done;done;done; python ${ENCODEPARSER} ${OUTPUTFOLDER};echo "Done")
